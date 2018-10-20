fc08806470076072a16f9fd6cf8f4c625aa3b9cc
Alejandra Rodriguez Marin <aleja830@gmail.com>
2018-10-19 23:53:41 -0500

    Contact List React

--- ejemplo_react/src/App.js
+++ ejemplo_react/src/App.js
@@ -1,10 +1,74 @@
 import React, { Component } from 'react';
 import './App.css';
+import guia from './guia-telefonica.png';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import PropTypes from 'prop-types';
 
 
 // Componente Padre
 // Stateful
 class App extends Component {
+    
+    //new
+    constructor(props){
+        super(props);
+        
+        this.state={ 
+           
+        };
+       
+    }
+    
+  render() {
+      //dentro de BrowserRouter debe tener 1 hijo , por eso está el div o sino sale error
+      //ya dentro de ese div se puede colocar todo lo que se quiera
+      //el exact dentro del Route es para que solo muestre ese componente
+      return (
+        
+            <div className="main">
+                <BrowserRouter>
+                    <div>
+                        <Navigation/>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/contactlist" component={ContactListSection}/>
+                    </div>
+                </BrowserRouter>
+           </div>
+      );
+  }
+}
+
+
+const Navigation=(props)=>{
+    
+        return(
+        
+            <nav>
+                <ul>
+                    <li><Link to={`/`}>Home </Link></li>
+                    <li><Link to={`/contactlist`}>Lista de contactos</Link></li>
+                </ul>
+            </nav>
+        
+        );
+    
+}
+
+const Home=(props)=>{
+    
+    return(
+        <div className='home'>
+            
+             <img src={guia} alt="guia telefonica"/>
+        
+        </div>
+    )
+    
+}
+
+//COPIA DE CLASE APP
+
+class ContactListSection extends Component {
     
     //new
     constructor(props){
@@ -18,7 +82,72 @@ class App extends Component {
             favorites:[]  
         };
         
+        
+        this.addToFavorites=this.addToFavorites.bind(this);
+        this.deleteContact=this.deleteContact.bind(this);
     }
+    
+    //método
+        addToFavorites(contact,titleList){
+            
+            if(titleList==="Todos"){
+
+                //filtramos el contacto que pasamos
+                const newAll=this.state.all.filter(c=>c.id.value!==contact.id.value);
+
+                //añadimos el contacto a favoritos
+                const newFavorites=this.state.favorites.concat(contact);
+
+                //console.log('index',contactIndex);
+
+                this.setState({
+                    all:newAll,
+                    favorites:newFavorites
+                });
+           
+            }else{
+
+                //filtramos el contacto que pasamos
+                const newFavorites=this.state.favorites.filter(c=>c.id.value!==contact.id.value);
+
+                //añadimos el contacto a Todos
+                const newAll=this.state.all.concat(contact);
+
+               
+                this.setState({
+                    all:newAll,
+                    favorites:newFavorites
+                });
+                
+            }
+            
+        }
+    
+    //metodo
+        deleteContact(contact,titleList){
+            
+            if(titleList==="Todos"){
+
+                //filtramos el contacto que pasamos
+                const newAll=this.state.all.filter(c=>c.id.value!==contact.id.value);
+
+                this.setState({
+                    all:newAll
+                 });
+                
+            }else{
+
+                //filtramos el contacto que pasamos
+                const newFavorites=this.state.favorites.filter(c=>c.id.value!==contact.id.value);
+
+                this.setState({
+                    favorites:newFavorites
+                 });
+                
+            }
+            
+        }
+    
     
     //new - consultar datos - Después de montar el componente en el DOM
 	
@@ -38,29 +167,29 @@ class App extends Component {
   
     }
     
-    componentDidUpdate(){
-        
-    }
-    
-    componentWillUnmount(){
-        
-        
-    }
     
     
   render() {
     return (
         
        //Cuando tengo componentes hermanitos que son los mismos, les pongo un key que los identifique
+        //añadir prop addToFavorites
       <div className="App">
-            
-            <ContactList contacts={this.state.all} title="Todos" key="1" />
-            <ContactList contacts={this.state.favorites} title="Favoritos" key='2'/>
+            <h1>mi lista de contactos</h1>
+            <div className="contact-list">
+                <ContactList contacts={this.state.all} title="Todos" key="1" addToFavorites={this.addToFavorites} deleteContact={this.deleteContact}  buttonName="Favorito"/>
+            </div>
+        
+            <div className="contact-list">
+                <ContactList contacts={this.state.favorites} title="Favoritos" key='2' addToFavorites={this.addToFavorites} deleteContact={this.deleteContact} buttonName="Todos"/>  
+            </div>
             
       </div>
     );
   }
 }
+
+//----------------------------------
 
 //new
 
@@ -73,27 +202,57 @@ const ContactList=(props)=>{
   return (
 		<div>
 			<h2>{props.title}</h2>
-			{ props.contacts.map(contact => <ContactCard key={contact.name.first} contact={contact} />) }
+			{ props.contacts.map(contact => <ContactCard key={contact.name.first} contact={contact} addToFavorites={props.addToFavorites} deleteContact={props.deleteContact} title={props.title} buttonName={props.buttonName}/>) }
 		</div>
       //por cada contacto se va a tener un ContactCard
 	);
+    
+    
 };
 
 
+//chequea que las props cumplan las condiciones especificadas
+ContactList.propTypes={
+        addToFavorites:PropTypes.func.isRequired,   //isRequired- es obligatorio
+        contacts:PropTypes.array.isRequired,
+        title:PropTypes.string.isRequired
+    }
+
+//por defecto se le pasa a la prop title lo definido acá. (sI no viene por ejemplo algo en esa prop)
+ContactList.defaultProps={
+    title:'Titulo'
+}
+
 // Stateful
 class ContactCard extends Component{
+    
+    constructor(props){      
+        super(props);
+        
+        this.onClickFavorites=this.onClickFavorites.bind(this);
+        this.onClickDelete=this.onClickDelete.bind(this);
+    }
+    
+    onClickFavorites(){
+        this.props.addToFavorites(this.props.contact,this.props.title);
+    }
+    
+    onClickDelete(){
+        this.props.deleteContact(this.props.contact,this.props.title);
+    }
     
     render(){
         return(
             <div className="contact-card">
     
                 <figure>
-                    <img src={this.props.contact.picture.medium} alt="Autor"/>
-                    <figcaption>{this.props.contact.name.first}</figcaption>
+                    <img src={this.props.contact.picture.large} alt="Autor"/>
+                    <figcaption>{this.props.contact.name.first+" "+this.props.contact.name.last}<span>{this.props.contact.phone}</span></figcaption>
+                    
 
                 </figure>
-                <button>Favorito</button>
-                <button>Eliminar</button>
+                <button onClick={()=>this.onClickFavorites()}>{this.props.buttonName}</button>
+                <button onClick={()=>this.onClickDelete()}>Eliminar</button>
     
             </div>
         );
@@ -104,7 +263,5 @@ class ContactCard extends Component{
     
     
 }
-
-
 
 export default App;
